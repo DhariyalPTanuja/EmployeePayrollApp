ccca2bdc500a3841452da5bb7a646c19c0779507
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-29 14:38:01 +0530

    using HTTP methods in UC1

--- src/main/java/com/payroll/employeepayrollsystemapp/controller/EmployeePayrollController.java
+++ src/main/java/com/payroll/employeepayrollsystemapp/controller/EmployeePayrollController.java
@@ -13,5 +13,21 @@ public class EmployeePayrollController {
     public String welcomeMsg() {
         return "welcome  to EmployeePayroll App";
     }
-
+    //UC1
+    @GetMapping("/get/{empId}")
+    public String getEmployeePayrollData(@PathVariable("empId") int empId){
+        return " get the data successful "  ;
+    }
+    @PostMapping("/post")
+    public String addEmployeePayrollData(@RequestBody EmployeePayrollDataModel emp){
+        return  emp.toString();
+    }
+    @PutMapping("/update")
+    public String updateEmployeePayrollData(@RequestBody EmployeePayrollDataModel emp){
+        return  "update employee data" + emp.toString();
+    }
+    @DeleteMapping("/delete/{empId}")
+    public String deleteEmployeePayrollData(@PathVariable EmployeePayrollDataModel emp){
+        return  "delete employee data for  " ;
+    }
 }
