d7f3e505ce1bc3d90266a9b6e3f8aa278f558d21
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-02 20:23:11 +0530

    create application.properties for dev and production

--- src/main/java/com/payroll/employeepayrollsystemapp/EmployeePayrollSystemAppApplication.java
+++ src/main/java/com/payroll/employeepayrollsystemapp/EmployeePayrollSystemAppApplication.java
@@ -1,13 +1,21 @@
 package com.payroll.employeepayrollsystemapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.naming.CannotProceedException;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSystemAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollSystemAppApplication.class, args);
+        log.info("Employee Payroll App started");
+//        log.info("Employee Payroll App started in {} Environment",
+//                context.getEnvironment().getProperty("environment"));
     }
 
 }
