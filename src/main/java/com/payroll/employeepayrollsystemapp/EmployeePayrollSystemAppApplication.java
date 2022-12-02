f11f968d0bf343b54159beb822032fa3fec152ca
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-02 16:07:32 +0530

    using Annotation @Slf4j at Class Level

--- src/main/java/com/payroll/employeepayrollsystemapp/EmployeePayrollSystemAppApplication.java
+++ src/main/java/com/payroll/employeepayrollsystemapp/EmployeePayrollSystemAppApplication.java
@@ -1,13 +1,16 @@
 package com.payroll.employeepayrollsystemapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSystemAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollSystemAppApplication.class, args);
-    }
 
+        log.info("Employee payroll start .. using annotation Slf4j");
+    }
 }
