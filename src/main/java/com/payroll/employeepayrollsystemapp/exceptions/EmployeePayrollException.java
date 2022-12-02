af449a7f20ca766cc01f67d2da376204052c9455
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-12-03 00:53:35 +0530

    To throw User Friendly errors in case employee Id is not found

---
+++ src/main/java/com/payroll/employeepayrollsystemapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.payroll.employeepayrollsystemapp.exceptions;
+
+public class EmployeePayrollException  extends RuntimeException{
+    public EmployeePayrollException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/payroll/employeepayrollsystemapp/service/EmpPayrollService.java
+++ src/main/java/com/payroll/employeepayrollsystemapp/service/EmpPayrollService.java
@@ -1,10 +1,13 @@
 package com.payroll.employeepayrollsystemapp.service;
 
 import com.payroll.employeepayrollsystemapp.dto.EmpPayrollDTO;
+import com.payroll.employeepayrollsystemapp.exceptions.EmployeePayrollException;
 import com.payroll.employeepayrollsystemapp.model.EmployeePayrollDataModel;
 import com.payroll.employeepayrollsystemapp.repository.EmpPayrollRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +16,18 @@ import java.util.Optional;
 public class EmpPayrollService implements IEmployeeService {
    @Autowired  //dependency injection  autowired repository object
     EmpPayrollRepo empRepository;
+   private List<EmployeePayrollDataModel> employeePayrollDataModelList = new ArrayList<>();
+   public List<EmployeePayrollDataModel> getEmployeePayrollData() {
+            return employeePayrollDataModelList;
+   }
+   //Exception throw
+    public EmployeePayrollDataModel getEmpData(int empId){
+//        EmployeePayrollDataModel empGetObj = empRepository.findById(id).get();
+        return employeePayrollDataModelList.stream()
+                .filter(empData -> empData.getEmpId() == empId)
+                .findFirst()
+                .orElseThrow(() -> new EmployeePayrollException("Employee Not found"));
+    }
     public String welcomeMsg() {
         return "welcome  to EmployeePayroll App";
     }
@@ -23,10 +38,10 @@ public class EmpPayrollService implements IEmployeeService {
         return empRepository.save(empModel);
     }
 
-    public EmployeePayrollDataModel getEmpData(int id){
-        EmployeePayrollDataModel empGetObj = empRepository.findById(id).get();
-        return empGetObj;
-    }
+//    public EmployeePayrollDataModel getEmpData(int id){
+//        EmployeePayrollDataModel empGetObj = empRepository.findById(id).get();
+//        return empGetObj;
+//    }
     public List<EmployeePayrollDataModel> getAllEmpData(){
         List<EmployeePayrollDataModel> empList = empRepository.findAll();
         return empList;
@@ -48,4 +63,5 @@ public class EmpPayrollService implements IEmployeeService {
     public void deleteEmpData(int id){
         empRepository.deleteById(id);
     }
+
 }
